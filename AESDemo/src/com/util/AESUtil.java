2a15fc5c1fcddfb287e2a00ceda84fead08400df
liutiejunhehe <liutiejun123a@163.com>
2019-07-18 16:15:55 +0800

    新增测试方法

--- AESDemo/src/com/util/AESUtil.java
+++ AESDemo/src/com/util/AESUtil.java
@@ -1,5 +1,81 @@
 package com.util;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import sun.misc.BASE64Decoder;
+
 public class AESUtil {
 
+	/*
+	 * 加密用的Key 可以用26个字母和数字组成 此处使用AES-128-CBC加密模式，key需要为16位。
+	 */
+	private String sKey;
+	private String ivParameter;
+
+	public AESUtil(String sKey, String ivParameter) {
+		this.sKey = ivParameter;
+		this.ivParameter = ivParameter;
+	}
+
+	/**
+	 * 加密
+	 * 
+	 * @param data
+	 * @return
+	 * @throws Exception
+	 */
+	public String encrypt(String data) throws Exception {
+		try {
+			Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+			int blockSize = cipher.getBlockSize();
+
+			byte[] dataBytes = data.getBytes();
+			int plaintextLength = dataBytes.length;
+			if (plaintextLength % blockSize != 0) {
+				plaintextLength = plaintextLength + (blockSize - (plaintextLength % blockSize));
+			}
+
+			byte[] plaintext = new byte[plaintextLength];
+			System.arraycopy(dataBytes, 0, plaintext, 0, dataBytes.length);
+
+			SecretKeySpec keyspec = new SecretKeySpec(sKey.getBytes(), "AES");
+			IvParameterSpec ivspec = new IvParameterSpec(ivParameter.getBytes());
+
+			cipher.init(Cipher.ENCRYPT_MODE, keyspec, ivspec);
+			byte[] encrypted = cipher.doFinal(plaintext);
+
+			return new sun.misc.BASE64Encoder().encode(encrypted);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * 解密
+	 * 
+	 * @param data
+	 * @return
+	 * @throws Exception
+	 */
+	public String desEncrypt(String data) throws Exception {
+		try {
+			byte[] encrypted1 = new BASE64Decoder().decodeBuffer(data);
+
+			Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+			SecretKeySpec keyspec = new SecretKeySpec(sKey.getBytes(), "AES");
+			IvParameterSpec ivspec = new IvParameterSpec(ivParameter.getBytes());
+
+			cipher.init(Cipher.DECRYPT_MODE, keyspec, ivspec);
+
+			byte[] original = cipher.doFinal(encrypted1);
+			String originalString = new String(original);
+			return originalString;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
